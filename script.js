9782241f044079c495f02c98a5c40e796b46eed4
Ashim <63529015+ashimsap@users.noreply.github.com>
2023-12-07 20:23:20 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contact 10px;
+-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById```javascript
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById('name').value}

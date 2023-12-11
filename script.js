c11c5b0f225c5691f141a616e47a5e36ca672f65
Ashim <63529015+ashimsap@users.noreply.github.com>
2023-12-11 21:43:01 +0545

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('contact 10px;
--form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    let name = document.getElementById```javascript
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    let name = document.getElementById('name').value}

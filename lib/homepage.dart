2026877f897bdc0414a2b3ad1b94886625e4acea
Ashim <ashimsap@gmail.com>
2024-12-10 23:20:41 +0545

    update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -6,9 +6,40 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.grey[700],
       appBar: AppBar(
-        title: Text("W E L C O M E", style: TextStyle(color: Colors.blue),),
+        backgroundColor: Colors.grey[700],
+        title: Text("W E L C O M E", style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Text("this is the body",textAlign: TextAlign.center, style: TextStyle(color: Colors.white),),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade700,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow:[
+                    BoxShadow(
+                    offset: Offset(-5, -5),
+                    color: Colors.grey.shade500,
+                    blurRadius: 12,
+                  ),
+                    BoxShadow(
+                      offset: Offset(5, 5),
+                      color: Colors.grey.shade900,
+                      blurRadius: 12,
+                    )],
+                ),
+                width: 150,
+                height: 150,
+              ),
+            )
+        ],),
       ),
     );
   }

bd4e67a00cb4cb53e022123495ef8ff0eb87deee
Ashim <ashimsap@gmail.com>
2024-12-11 19:15:32 +0545

    update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,34 +9,47 @@ class Homepage extends StatelessWidget {
       backgroundColor: Colors.grey[700],
       appBar: AppBar(
         backgroundColor: Colors.grey[700],
-        title: Text("W E L C O M E", style: TextStyle(color: Colors.white),),
+        title: const Text("W E L C O M E", style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold, fontSize: 28),),
         centerTitle: true,
       ),
       body: Container(
+    decoration: const BoxDecoration(
+    gradient: LinearGradient(colors: [Colors.blue,Colors.pinkAccent,Colors.red,Colors.green]),),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-          Text("this is the body",textAlign: TextAlign.center, style: TextStyle(color: Colors.white),),
+          const Text("this is the body",textAlign: TextAlign.center, style: TextStyle(color: Colors.black),),
             Padding(
               padding: const EdgeInsets.all(15.0),
               child: Container(
+
+                height: 150,
                 decoration: BoxDecoration(
-                  color: Colors.grey.shade700,
+                  color: Colors.red,
                   borderRadius: BorderRadius.circular(12),
                   boxShadow:[
                     BoxShadow(
-                    offset: Offset(-5, -5),
+                    offset: const Offset(-5, -5),
                     color: Colors.grey.shade500,
                     blurRadius: 12,
                   ),
                     BoxShadow(
-                      offset: Offset(5, 5),
+                      offset: const Offset(5, 5),
                       color: Colors.grey.shade900,
                       blurRadius: 12,
                     )],
                 ),
-                width: 150,
-                height: 150,
+                child: const Center(child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      Text("H  E  L  L  O, "),
+                      Text("MadaFaka")
+                    ],
+                  ),
+                )),
               ),
             )
         ],),
